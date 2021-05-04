6cc7c771b93d14542a2c73fc1404913e065e94f6
Zo3-L <73539290+Zo3-L@users.noreply.github.com>
2021-05-03 21:04:14 -0400

    Create bash.js

---
+++ bash.js
@@ -0,0 +1,7 @@
+process.stdout.write('prompt > ');
+process.stdin.on('data', (data) => {
+    const pwd = data.toString().trim();
+  
+    process.stdout.write(process.cwd());
+    process.stdout.write('\nprompt > ');
+});
